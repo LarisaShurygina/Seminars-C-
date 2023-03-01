78552847a8f1a965f2f851e456f5e10d334b1c91
Larisa Shurygina <larisashurygina@gmail.com>
2023-03-01 13:48:48 +0300

    Домашнее задание 4

---
+++ homeworks/homework4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+int FindDegree(int A, int B)
+{
+    int degree = 0;
+    
+    Math.Pow(A, B);
+Console.WriteLine("A^B = " + Math.Pow(A, B));
+    return degree;
+}
+
+Console.WriteLine("Input A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+FindDegree(A, B);
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
